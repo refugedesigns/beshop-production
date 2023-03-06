dfce25f86b8d9c44bd0a737a0a563c691a461855
Erasmus <refugedesigns17@gmail.com>
2023-03-06 12:12:44 +0000

    initial commit - setup tailwind and mui

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import { StyledEngineProvider } from '@mui/material'
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import theme from '@/theme/theme';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      <Component {...pageProps} />
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
 }
