1c86b9b0b82935c28fb9cd00fb683f2664c785a8
Erasmus <refugedesigns17@gmail.com>
2023-06-02 09:05:21 +0000

    tried fixing policy issue

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use(
         "js.stripe.com",
         "events.mapbox.com",
         "goshop-vercel.vercel.app",
-        "localhost:8000"
+        "http://localhost:8000"
       ],
       "worker-src": ["'self'", "'unsafe-inline'", "'unsafe-eval'", "blob:"],
     },
